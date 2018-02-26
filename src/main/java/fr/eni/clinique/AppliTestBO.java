621d8583e02f5959fbdb0cb06898a0d94796edef
MorganPot <36854688+MorganPot@users.noreply.github.com>
2018-02-26 17:06:06 +0100

    ortho

--- src/main/java/fr/eni/clinique/AppliTestBO.java
+++ src/main/java/fr/eni/clinique/AppliTestBO.java
@@ -17,7 +17,7 @@ public class AppliTestBO {
 	public static void main(String[] args) {
 		GregorianCalendar cal = new GregorianCalendar();
 		cal.setTime(new java.util.Date());
-		Client cli1 = new Client(1, "NOEL","Pierre-Axel","1 Chateau Eraudière","La Vigne","44000","Nantes","0645743525","Axa","pierreaxel.noel@gmail.com","remarque", true);
+		Client cli1 = new Client(1, "NOEL","Pierre-Axel","1 Chateau EraudiÃ¨re","La Vigne","44000","Nantes","0645743525","Axa","pierreaxel.noel@gmail.com","remarque", true);
 		Agenda agenda1 = new Agenda(1, cal, 1);
 		Animal animal1 = new Animal(1, "Diwan", "Male", "Marron", "Teckel", "Chiendelacasse", 1, "LeBGdu57", "VoldeVoiture",true);
 		Personnel pers1 = new Personnel(1, "MorganLeBGDu44", "azertyui", "Sec", true);
@@ -27,7 +27,5 @@ public class AppliTestBO {
 		System.out.println(animal1);
 		System.out.println(pers1);
 		System.out.println(race1);
-
-		
 	}
 }
