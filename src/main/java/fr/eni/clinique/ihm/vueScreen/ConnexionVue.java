9cd879f9e9b7011a71d49c4677f672abc72a35e9
MorganPot <36854688+MorganPot@users.noreply.github.com>
2018-02-27 17:25:44 +0100

    Reset

--- src/main/java/fr/eni/clinique/ihm/vueScreen/ConnexionVue.java
+++ src/main/java/fr/eni/clinique/ihm/vueScreen/ConnexionVue.java
@@ -30,15 +30,8 @@ public class ConnexionVue extends JFrame {
 	/**
 	 * 
 	 */
-<<<<<<< HEAD
 	private static final long serialVersionUID = 7833478608680411934L;
 	
-=======
-	private static final long serialVersionUID = 6893577698630411035L;
-	public JFrame frame;
-	private JTextField textField;
-	private JPasswordField passwordField;
->>>>>>> a5595356df6ceed716f4166f2dbc01291251b432
 	private ConnexionDAOJdbcImpl connexion = ConnexionDAOJdbcImpl.getInstance();
     private ConnexionActionListener actionListener;
     
@@ -76,46 +69,6 @@ public class ConnexionVue extends JFrame {
 	 * Launch the application.
 	 */
 	private void initialize() {
-<<<<<<< HEAD
-=======
-		frame = new JFrame();
-		frame.setBounds(200, 200, 400, 200);
-		frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.setResizable(false);
-		
-		JLabel lblNewLabel = new JLabel("Nom");
-		lblNewLabel.setBounds(10, 31, 91, 23);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JLabel erreur = new JLabel("");
-		erreur.setForeground(Color.red);
-		erreur.setBounds(10,0, 300, 23);
-		frame.getContentPane().add(erreur);
-		
-		JLabel lblPassword = new JLabel("Mot de passe");
-		lblPassword.setBounds(10, 65, 91, 14);
-		frame.getContentPane().add(lblPassword);
-		
-		textField = new JTextField();
-		textField.setBounds(111, 32, 115, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(111, 62, 115, 20);
-		frame.getContentPane().add(passwordField);
-		passwordField.setColumns(10);
-		
-		JButton btnLogin = new JButton("Valider");
-		btnLogin.setBounds(111, 93, 115, 23);
-		frame.getContentPane().add(btnLogin);
-		
-		ImageIcon icone = new ImageIcon("images/fond_frame.jpg");
-		frame.setIconImage(new ImageIcon("images/fond_frame.jpg").getImage());
-        frame.setSize(200,200);
-        frame.setVisible(true);
->>>>>>> a5595356df6ceed716f4166f2dbc01291251b432
 		
 		panel = new JPanel();
         panel.setOpaque(true);
