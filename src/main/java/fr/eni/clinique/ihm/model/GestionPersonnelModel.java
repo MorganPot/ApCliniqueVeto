1de35b9132cd75c718702f09b5bcb42e9d2da08d
pnoel <pierreaxel.noel@gmail.com>
2018-02-28 17:22:26 +0100

    GestionPersonnelModel

--- src/main/java/fr/eni/clinique/ihm/model/GestionPersonnelModel.java
+++ src/main/java/fr/eni/clinique/ihm/model/GestionPersonnelModel.java
@@ -1,21 +1,25 @@
 package fr.eni.clinique.ihm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
 import fr.eni.clinique.bo.Personnel;
 
-public class GestionPersonnelModel {
-public GestionPersonnelModel(){
-		
+public class GestionPersonnelModel extends Observable{
+	private List<Personnel> lesPersonnels = new ArrayList<>();
+	
+	public void chargerPersonnel(List<Personnel> lesPersonnels) {
+		this.lesPersonnels.clear();
+        this.lesPersonnels.addAll(lesPersonnels);
+        
+        setChanged();
+        notifyObservers();
+        clearChanged();
 	}
 	
-	public void AddPersonnel(Personnel personnels){
-		
-	}
+	public void addPersonnel(Personnel personnel) {
+        lesPersonnels.add(personnel);
+    }
 	
-	public void DeletePersonnel(int codePers){
-		
-	}
-	
-	public void RéinitialiserPersonnel(int codePers){
-		
-	}
 }
