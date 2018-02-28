16c10869295b8415ab1ffa3bbdb94527bbb8fabd
pnoel <pierreaxel.noel@gmail.com>
2018-02-28 16:10:42 +0100

    ajout managerfactory

--- src/main/java/fr/eni/clinique/bll/factory/ManagerFactory.java
+++ src/main/java/fr/eni/clinique/bll/factory/ManagerFactory.java
@@ -1,7 +1,11 @@
 package fr.eni.clinique.bll.factory;
 
+import fr.eni.clinique.bll.manager.ClientManager;
 import fr.eni.clinique.bll.manager.LoginManager;
+import fr.eni.clinique.bll.manager.PersonnelManager;
+import fr.eni.clinique.bll.manager.impl.ClientManagerImpl;
 import fr.eni.clinique.bll.manager.impl.LoginManagerImpl;
+import fr.eni.clinique.bll.manager.impl.PersonnelManagerImpl;
 
 /**
  * Manager Factory.
@@ -18,5 +22,13 @@ public class ManagerFactory {
      */
     public static LoginManager loginManager() {
         return LoginManagerImpl.getInstance();
+    }
+    
+    public static ClientManager clientManager() {
+        return ClientManagerImpl.getInstance();
+    }
+    
+    public static PersonnelManager personnelManager() {
+        return PersonnelManagerImpl.getInstance();
     }
 }
