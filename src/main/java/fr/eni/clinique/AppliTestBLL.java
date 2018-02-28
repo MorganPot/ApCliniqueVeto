4da12a6be60cc55fd9d3bb830335f9574f305eb6
MorganPot <36854688+MorganPot@users.noreply.github.com>
2018-02-28 12:25:35 +0100

    ModifPerso

--- src/main/java/fr/eni/clinique/AppliTestBLL.java
+++ src/main/java/fr/eni/clinique/AppliTestBLL.java
@@ -3,8 +3,8 @@ package fr.eni.clinique;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.eni.clinique.bll.exception.LoginException;
-import fr.eni.clinique.bll.factory.LoginFactory;
+import fr.eni.clinique.bll.exception.ManagerException;
+import fr.eni.clinique.bll.factory.ManagerFactory;
 import fr.eni.clinique.bo.Personnel;
 import fr.eni.clinique.common.exception.TechnicalException;
 import fr.eni.clinique.ihm.model.ConnexionModel;
@@ -13,7 +13,7 @@ public class AppliTestBLL {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AppliTestBLL.class);
     
-	public static void main(String[] args) throws LoginException {
+	public static void main(String[] args) throws ManagerException {
 		// TODO Auto-generated method stub
 		ConnexionModel model = new ConnexionModel();
 		
@@ -22,13 +22,13 @@ public class AppliTestBLL {
 				System.out.println("win");
 			}
 			else{
-				for (Personnel personnel : LoginFactory.loginManager().getPerso()) {
+				for (Personnel personnel : ManagerFactory.loginManager().getPerso()) {
 					System.out.println(personnel.getNom());
 					System.out.println(personnel.getMotPasse());
 				}
 				System.out.println("lose");
 			}
-		} catch (LoginException e) {
+		} catch (ManagerException e) {
 			// TODO Auto-generated catch block
 			throw new TechnicalException("Erreur Technique", e);
 		}
